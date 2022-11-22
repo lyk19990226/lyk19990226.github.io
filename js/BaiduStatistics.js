126d7212ea8667123880f446d1b3081045647342
lyk19990226 <1272198474@qq.com>
2022-11-22 23:58:46 +0800

    版本更新221026-01

---
+++ js/BaiduStatistics.js
@@ -0,0 +1,11 @@
+// 百度统计代码
+// 其中 id 可以换成自己的百度统计 id，登录百度统计查看网站浏览记录
+// https://tongji.baidu.com/web/homepage/index
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?64b16308d7290574fae05a087fe09570";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
